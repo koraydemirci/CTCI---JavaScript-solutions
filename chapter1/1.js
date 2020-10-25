0002e7e701cfe0d420f6204fe77e8ee0ef01675d
koraydemirci <koraydemirci@gmail.com>
2020-10-25 18:53:51 +0300

    add chapter 1 & question 1.1

---
+++ chapter1/1.js
@@ -0,0 +1,36 @@
+/**
+ CTCI - 1.1 
+ 
+Is Unique:
+Implement an algorithm to determine if a string has all unique characters.  
+What if you cannot use additional data structures?
+I: string
+O: boolean
+C: no additional data structures, optimize
+E: empty string
+time complexity: linear - O(n)
+space complexity: linear - O(n)
+*/
+
+let isUnique = (str) => {
+  //check each character and store in a hash table
+  //if we find that the character is already there, return false
+  //when done checking, return true
+  const hash = {};
+
+  for (let s of str) {
+    if (hash[s]) {
+      return false;
+    }
+    hash[s] = true;
+  }
+
+  return true;
+};
+
+console.log(isUnique("s") === true);
+console.log(isUnique("") === true);
+console.log(isUnique("ss") === false);
+console.log(isUnique("stories") === false);
+console.log(isUnique("rainbow") === true);
+console.log(isUnique("chirpingmermaid") === false);
